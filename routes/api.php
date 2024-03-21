f277b5a3c838376d5e0ec55ab43593ddb64c6cf1
agungtrisutaji <agung.trisutaji.aprian@gmail.com>
2024-03-21 22:52:15 +0700

    add get method for posts

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('post', [PostController::class, 'index']);
+Route::get('post/{id}', [PostController::class, 'show']);
+
+Route::post('post', [PostController::class, 'store']);
